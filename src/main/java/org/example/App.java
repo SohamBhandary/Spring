9ea58ff5aa8298d6929e4d50e27b32c3db86b717
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-04 21:40:49 +0530

    Spring bean Xml Congif

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
+        Alien a = (Alien) context.getBean("alien");
+        a.code();
+
+
+
+
+    }
+}
